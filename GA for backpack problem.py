bfb18d189e767a00822798144f7c1495741e1b64
Hengxi Zhang <72500446+Dylan2020THU@users.noreply.github.com>
2023-11-28 09:48:59 +0800

    Update GA for backpack problem.py

--- GA for backpack problem.py
+++ GA for backpack problem.py
@@ -1,5 +1,5 @@
 # Born: 2023-11-20
-# Update: 2023-11-26
+# Update: 2023-11-27
 # Dylan
 # Genetic algorithm for backpack problem
 
@@ -37,77 +37,18 @@ with open(file_path_s, 'r') as file:
     # Convert each line to an integer and store in a list
     optima_sol = [int(line.strip()) for line in file]
 
-m = 100  # Number of chromosomes or solutions
-p = 0.5  # Probability of mutation
-t_max = 30  # Number of generations
-
 # Generate random items
 # items = np.zeros((n, 2))
 # for i in range(n):
 #     items[i, 0] = random.randint(1, 5)  # Weight
 #     items[i, 1] = random.randint(100, 200)  # Value
-
-# Generate random chromosomes
-chromosomes = np.zeros((m, n))
-for i in range(m):
-    chromosomes[i, :] = np.random.randint(2, size=n)
-
-# Calculate fitness and weight of chromosomes
-weight = np.zeros(m)
-fitness = np.zeros(m)
-for i in range(m):
-    weight[i] = np.dot(chromosomes[i, :], items[:, 0])
-    fitness[i] = np.dot(chromosomes[i, :], items[:, 1])
-    # judge if weight is over capacity
-    if weight[i] > capacity:
-        fitness[i] = 0
-# Sort chromosomes by fitness
-idx = np.argsort(fitness)  # Get indices of sorted chromosomes
-chromosomes_sort = chromosomes[idx, :]  # Sort chromosomes
-init_fitness_sort = fitness[idx]  # Sort fitness
-weight_sort = weight[idx]  # Sort weight
-
-print('Initial fitness:\n', init_fitness_sort)
-
-list_best_chromosom_fitness = np.empty(t_max)
-# start with an initial time t = 0
-for t in range(t_max):
-    print("Generation: ", t + 1)
-    # generate a new population with the same size as the initial population
-    chromosomes_new = np.zeros((m, n))  # m chromosomes with n genes in each chromosome
-
-    # Crossover
+def ga(m, p, t_max):
+    # Generate random chromosomes
+    chromosomes = np.zeros((m, n))
     for i in range(m):
-        # Select a sub-population for offspring production
-        # Select the best 20 chromosomes
-        size_best = 20
-        chromosomes_best = chromosomes_sort[-size_best:, :]
-        # repeat the crossover to get the new population using the best 20 chromosomes
-        for j in range(size_best):
-            # randomly select two chromosomes from the best 20 chromosomes
-            idx_parent1 = random.randint(0, size_best - 1)
-            # select another parent chromosome except the first
-            best_list = list(range(size_best))
-            best_list.remove(idx_parent1)
-            idx_parent2 = random.choice(best_list)
-            # randomly select a crossover point
-            crossover_point = random.randint(0, n - 1)
-            # crossover
-            chromosomes_new[i, :crossover_point] = chromosomes_best[idx_parent1, :crossover_point]
-            chromosomes_new[i, crossover_point:] = chromosomes_best[idx_parent2, crossover_point:]
+        chromosomes[i, :] = np.random.randint(2, size=n)
 
-    # Mutation
-    # randomly select a chromosome
-    for i in range(m):
-        # randomly select a gene
-        d = random.randint(0, n - 1)
-        # mutation
-        if random.random() < p:
-            chromosomes_new[i, d] = 1 - chromosomes_new[i, d]
-    # Update chromosomes
-    chromosomes = chromosomes_new
-
-    # Calculate and sort fitness of chromosomes in the new population
+    # Calculate fitness and weight of chromosomes
     weight = np.zeros(m)
     fitness = np.zeros(m)
     for i in range(m):
@@ -119,21 +60,85 @@ for t in range(t_max):
     # Sort chromosomes by fitness
     idx = np.argsort(fitness)  # Get indices of sorted chromosomes
     chromosomes_sort = chromosomes[idx, :]  # Sort chromosomes
-    fitness_sort = fitness[idx]  # Sort fitness
+    init_fitness_sort = fitness[idx]  # Sort fitness
     weight_sort = weight[idx]  # Sort weight
 
-    list_best_chromosom_fitness[t] = fitness_sort[-1]
+    print('Initial fitness:\n', init_fitness_sort)
 
-    print("Best fitness: ", fitness_sort[-1])
-print("Allocation scheme: ", chromosomes_sort[-1])
+    fitness_list = np.empty(t_max)
+    # start with an initial time t = 0
+    for t in range(t_max):
+        print("Generation: ", t + 1)
+        # generate a new population with the same size as the initial population
+        chromosomes_new = np.zeros((m, n))  # m chromosomes with n genes in each chromosome
+
+        # Crossover
+        for i in range(m):
+            # Select a sub-population for offspring production
+            # Select the best 20 chromosomes
+            size_best = 20
+            chromosomes_best = chromosomes_sort[-size_best:, :]
+            # repeat the crossover to get the new population using the best 20 chromosomes
+            for j in range(size_best):
+                # randomly select two chromosomes from the best 20 chromosomes
+                idx_parent1 = random.randint(0, size_best - 1)
+                # select another parent chromosome except the first
+                best_list = list(range(size_best))
+                best_list.remove(idx_parent1)
+                idx_parent2 = random.choice(best_list)
+                # randomly select a crossover point
+                crossover_point = random.randint(0, n - 1)
+                # crossover
+                chromosomes_new[i, :crossover_point] = chromosomes_best[idx_parent1, :crossover_point]
+                chromosomes_new[i, crossover_point:] = chromosomes_best[idx_parent2, crossover_point:]
+
+        # Mutation
+        # randomly select a chromosome
+        for i in range(m):
+            # randomly select a gene
+            d = random.randint(0, n - 1)
+            # mutation
+            if random.random() < p:
+                chromosomes_new[i, d] = 1 - chromosomes_new[i, d]
+        # Update chromosomes
+        chromosomes = chromosomes_new
+
+        # Calculate and sort fitness of chromosomes in the new population
+        weight = np.zeros(m)
+        fitness = np.zeros(m)
+        for i in range(m):
+            weight[i] = np.dot(chromosomes[i, :], items[:, 0])
+            fitness[i] = np.dot(chromosomes[i, :], items[:, 1])
+            # judge if weight is over capacity
+            if weight[i] > capacity:
+                fitness[i] = 0
+        # Sort chromosomes by fitness
+        idx = np.argsort(fitness)  # Get indices of sorted chromosomes
+        chromosomes_sort = chromosomes[idx, :]  # Sort chromosomes
+        fitness_sort = fitness[idx]  # Sort fitness
+        weight_sort = weight[idx]  # Sort weight
+
+        fitness_list[t] = fitness_sort[-1]
+
+        print(f"{t+1}-th Fitness: ", fitness_sort[-1])
+    return init_fitness_sort[-1], chromosomes_sort[-1], fitness_list
+
+
+num_chromosome = 100  # Number of chromosomes or solutions
+prob_mutation = 0.05  # Probability of mutation
+generation_max = 30  # Number of generations
+
+init_sol, ga_sol, ga_fitness_list = ga(num_chromosome, prob_mutation, generation_max)
+
+print("Allocation scheme: ", ga_sol)
 print('Offical optima: ', optima_sol)
 
 # Plot best fitness
 plt.figure()
-plt.scatter(0, init_fitness_sort[-1], c='r', marker='o')
-plt.plot(list_best_chromosom_fitness, c='b', marker='^')
+plt.scatter(0, init_sol, c='r', marker='o')
+plt.plot(ga_fitness_list, c='b', marker='^')
 plt.xlabel("Generation")
-plt.ylabel("Best fitness")
+plt.ylabel("Fitness")
 plt.legend(["Initial fitness", "Best fitness"])
 plt.savefig("GA for backpack problem.png")
 plt.show()

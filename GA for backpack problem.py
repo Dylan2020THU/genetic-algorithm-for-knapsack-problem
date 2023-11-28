f2c40595ca9c52f03d3fcf5ed232e23291f9da0e
Hengxi Zhang <72500446+Dylan2020THU@users.noreply.github.com>
2023-11-28 11:29:41 +0800

    random fitness is added for comparison

--- GA for backpack problem.py
+++ GA for backpack problem.py
@@ -1,52 +1,18 @@
+# Project name: Genetic algorithm for backpack problem
 # Born: 2023-11-20
 # Update: 2023-11-27
-# Dylan
-# Genetic algorithm for backpack problem
+# Author: Dylan
 
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-random.seed(2023)
 
-# Parameters
-file_path_c = r'C:\Users\Administrator\Desktop\genetic-algorithm-for-V2X-resource-allocation-main\p01\p01_c.txt'
-file_path_p = r'C:\Users\Administrator\Desktop\genetic-algorithm-for-V2X-resource-allocation-main\p01\p01_p.txt'
-file_path_w = r'C:\Users\Administrator\Desktop\genetic-algorithm-for-V2X-resource-allocation-main\p01\p01_w.txt'  # Replace with your actual file path
-file_path_s = r'C:\Users\Administrator\Desktop\genetic-algorithm-for-V2X-resource-allocation-main\p01\p01_s.txt'
-
-with open(file_path_c, 'r') as file:
-    capacity = int(file.read())
-
-n = 0  # the amount of objects
-# Open the file and read line by line
-with open(file_path_p, 'r') as file:
-    for line in file:
-        n += 1
-
-items = np.zeros((n, 2))
-# Open the file and read the lines
-with open(file_path_w, 'r') as file:
-    # Convert each line to an integer and store in a list
-    items[:, 0] = [int(line.strip()) for line in file]
-with open(file_path_p, 'r') as file:
-    # Convert each line to an integer and store in a list
-    items[:, 1] = [int(line.strip()) for line in file]
-
-with open(file_path_s, 'r') as file:
-    # Convert each line to an integer and store in a list
-    optima_sol = [int(line.strip()) for line in file]
-
-# Generate random items
-# items = np.zeros((n, 2))
-# for i in range(n):
-#     items[i, 0] = random.randint(1, 5)  # Weight
-#     items[i, 1] = random.randint(100, 200)  # Value
 def ga(m, p, t_max):
     # Generate random chromosomes
-    chromosomes = np.zeros((m, n))
+    chromosomes = np.zeros((m, num_item))
     for i in range(m):
-        chromosomes[i, :] = np.random.randint(2, size=n)
+        chromosomes[i, :] = np.random.randint(2, size=num_item)
 
     # Calculate fitness and weight of chromosomes
     weight = np.zeros(m)
@@ -70,7 +36,7 @@ def ga(m, p, t_max):
     for t in range(t_max):
         print("Generation: ", t + 1)
         # generate a new population with the same size as the initial population
-        chromosomes_new = np.zeros((m, n))  # m chromosomes with n genes in each chromosome
+        chromosomes_new = np.zeros((m, num_item))  # m chromosomes with n genes in each chromosome
 
         # Crossover
         for i in range(m):
@@ -87,7 +53,7 @@ def ga(m, p, t_max):
                 best_list.remove(idx_parent1)
                 idx_parent2 = random.choice(best_list)
                 # randomly select a crossover point
-                crossover_point = random.randint(0, n - 1)
+                crossover_point = random.randint(0, num_item - 1)
                 # crossover
                 chromosomes_new[i, :crossover_point] = chromosomes_best[idx_parent1, :crossover_point]
                 chromosomes_new[i, crossover_point:] = chromosomes_best[idx_parent2, crossover_point:]
@@ -96,7 +62,7 @@ def ga(m, p, t_max):
         # randomly select a chromosome
         for i in range(m):
             # randomly select a gene
-            d = random.randint(0, n - 1)
+            d = random.randint(0, num_item - 1)
             # mutation
             if random.random() < p:
                 chromosomes_new[i, d] = 1 - chromosomes_new[i, d]
@@ -120,25 +86,81 @@ def ga(m, p, t_max):
 
         fitness_list[t] = fitness_sort[-1]
 
-        print(f"{t+1}-th Fitness: ", fitness_sort[-1])
+        print(f"{t + 1}-th GA fitness: ", fitness_sort[-1])
     return init_fitness_sort[-1], chromosomes_sort[-1], fitness_list
 
 
-num_chromosome = 100  # Number of chromosomes or solutions
-prob_mutation = 0.05  # Probability of mutation
-generation_max = 30  # Number of generations
+if __name__ == "__main__":
 
-init_sol, ga_sol, ga_fitness_list = ga(num_chromosome, prob_mutation, generation_max)
+    random.seed(2023)
 
-print("Allocation scheme: ", ga_sol)
-print('Offical optima: ', optima_sol)
+    # Problem settings
+    file_path_c = r'C:\Users\Administrator\Desktop\genetic-algorithm-for-V2X-resource-allocation-main\p01\p01_c.txt'
+    file_path_p = r'C:\Users\Administrator\Desktop\genetic-algorithm-for-V2X-resource-allocation-main\p01\p01_p.txt'
+    file_path_w = r'C:\Users\Administrator\Desktop\genetic-algorithm-for-V2X-resource-allocation-main\p01\p01_w.txt'  # Replace with your actual file path
+    file_path_s = r'C:\Users\Administrator\Desktop\genetic-algorithm-for-V2X-resource-allocation-main\p01\p01_s.txt'
 
-# Plot best fitness
-plt.figure()
-plt.scatter(0, init_sol, c='r', marker='o')
-plt.plot(ga_fitness_list, c='b', marker='^')
-plt.xlabel("Generation")
-plt.ylabel("Fitness")
-plt.legend(["Initial fitness", "Best fitness"])
-plt.savefig("GA for backpack problem.png")
-plt.show()
+    # Load the capacity
+    with open(file_path_c, 'r') as file:
+        capacity = int(file.read())
+
+    # Load the amount of objects
+    num_item = 0
+    with open(file_path_p, 'r') as file:  # Open the file and read the lines
+        for line in file:
+            num_item += 1
+
+    # Load the values and weights of items
+    items = np.zeros((num_item, 2))
+    with open(file_path_w, 'r') as file:
+        items[:, 0] = [int(line.strip()) for line in file]  # Convert each line to an integer and store in a list
+    with open(file_path_p, 'r') as file:
+        items[:, 1] = [int(line.strip()) for line in file]
+
+    # Load the optimal solution
+    with open(file_path_s, 'r') as file:
+        optima_sol = [int(line.strip()) for line in file]
+
+    # Generate random items
+    # items = np.zeros((num_item, 2))
+    # for i in range(num_item):
+    #     items[i, 0] = random.randint(1, 5)  # Weight
+    #     items[i, 1] = random.randint(100, 200)  # Value
+
+    # GA fitness
+    num_chromosome = 100  # Number of chromosomes or solutions
+    prob_mutation = 0.05  # Probability of mutation
+    generation_max = 30  # Number of generations
+
+    init_sol, ga_sol, ga_fitness_list = ga(num_chromosome, prob_mutation, generation_max)
+
+    print("Allocation scheme: ", ga_sol)
+    print('Offical optima: ', optima_sol)
+
+    # Random fitness
+    random_fitness_list = np.empty(shape=generation_max)
+    for t in range(generation_max):
+        # Generate random chromosomes
+        random_chromosomes = np.zeros((num_chromosome, num_item))
+        random_weight = np.zeros(num_chromosome)
+        random_fitness = np.zeros(num_chromosome)
+        for i in range(num_chromosome):
+            random_chromosomes[i, :] = np.random.randint(2, size=num_item)
+            random_fitness[i] = np.dot(random_chromosomes[i, :], items[:, 1])
+            random_weight[i] = np.dot(random_chromosomes[i, :], items[:, 0])
+            # judge if weight is over capacity
+            if random_weight[i] > capacity:
+                random_fitness[i] = 0
+        random_fitness_list[t] = np.average(random_fitness)
+        print(f"{t}-th random fitness: ", random_fitness_list[t])
+
+    # Plot the fitness
+    plt.figure()
+    plt.scatter(0, init_sol, c='r', marker='o')
+    plt.plot(ga_fitness_list, c='b', marker='*')
+    plt.plot(random_fitness_list, c='k', marker='^')
+    plt.xlabel("Generation")
+    plt.ylabel("Fitness")
+    plt.legend(["Initial fitness", "GA fitness", "Random fitness"])
+    plt.savefig("GA for backpack problem.png")
+    plt.show()
